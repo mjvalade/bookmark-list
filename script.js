6a0e2ea403985a3c62c3028025f6f582d099cc0e
MaryJane Valade <mjvalade@gmail.com>
2016-07-12 14:01:49 -0600

    Initial Files

---
+++ script.js
@@ -0,0 +1,11 @@
+// Two input fields:
+//    input: title of the bookmark
+//    input: URL that the bookmark should link to
+//
+// One for creating the bookmark and adding it to the page
+//
+// A section for all of the created bookmarks; each bookmark should display:
+//    The title of the bookmark
+//    The URL of the bookmark (this should be clickable and link to the URL)
+//    button: “Mark as Read”
+//    button: “Remove” the bookmark
