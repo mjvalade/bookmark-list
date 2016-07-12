5fc7b123a8516c22da13304bb790f5cb79f3c628
MaryJane Valade <mjvalade@gmail.com>
2016-07-12 16:41:41 -0600

    initial startup phase 1

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
-// Two input fields:
-//    input: title of the bookmark
-//    input: URL that the bookmark should link to
-//
-// One for creating the bookmark and adding it to the page
-//
-// A section for all of the created bookmarks; each bookmark should display:
-//    The title of the bookmark
-//    The URL of the bookmark (this should be clickable and link to the URL)
-//    button: “Mark as Read”
-//    button: “Remove” the bookmark
+var submitButton = document.querySelector('.submit-button');
+var inputTitle = document.querySelector('.input-title-field');
+var displayTitle = document.querySelector('.display-title-field');
+var inputUrl = document.querySelector('.input-url-field');
+var displayUrl = document.querySelector('.display-url-field');
+// var  = document.querySelector(''); *href*
+
+
+function saveBookmark() {
+  displayTitle.innerText = inputTitle.value;
+  displayUrl.innerText = inputUrl.value;
+  // .innerHTML = inputUrl.value;
+}
+
+submitButton.addEventListener('click', function () {
+  saveBookmark();
+  // $('.list1').prepend( $('.secondary-content'));
+});
+
+$('.read-button').on('click', function () {
+  $('.list1').toggleClass("red");
+});
+
+$('.remove-button').on('click', function () {
+  $('.display-title-field, .display-url-field').empty();
+  $('.list1').removeClass("red");
+});
